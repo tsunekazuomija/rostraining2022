077950af852eef75aa382297025d03e39c7df960
Tsunekazu Omija <mijamija0301@gmail.com>
2022-09-18 15:57:16 +0900

    Improve the test

--- test.bash
+++ test.bash
@@ -1,6 +1,17 @@
-#!/bin/bash -xv
+#!/bin/bash
+# SPDX-FileCopyrightText: 2022 Tsunekazu Omija
+# SPDX-License-Identifier: BSD-3-Clause
 
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
 out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng $LINENO
 
-[ "${out}" = 15 ]
-
+[ "$res" = 0 ] && echo OK
+exit $res
